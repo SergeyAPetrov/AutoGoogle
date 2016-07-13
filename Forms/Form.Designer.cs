2d96eb0ae5a8b789c3c49247950effb2babc9165
Sergey Petrov <Sergey.Petrov@confirmit.com>
2016-07-13 09:27:41 +0300

    Autogoogle initial implementaion

---
+++ Forms/Form.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Forms
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 238);
+            this.textBox1.TabIndex = 0;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "AutoGoogle";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.Click += new System.EventHandler(this.notifyIcon_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.textBox1);
+            this.Enabled = false;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form";
+            this.Text = "AutoGoogle";
+            this.Deactivate += new System.EventHandler(this.Form_Deactivate);
+            this.Resize += new System.EventHandler(this.Form_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ Forms/Form.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+using Forms.Properties;
+
+namespace Forms
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        KeyboardHook hook = new KeyboardHook();
+
+        public Form()
+        {
+            InitializeComponent();
+            hook.KeyPressed += hook_KeyPressed;
+            hook.RegisterHotKey(Forms.ModifierKeys.Control | Forms.ModifierKeys.Alt, (Keys)Enum.Parse(typeof(Keys), Settings.Default.Key));
+        }
+
+        void hook_KeyPressed(object sender, KeyPressedEventArgs e)
+        {
+            Thread.Sleep(Settings.Default.WaitTimeout);
+            SendKeys.SendWait("^c");
+            Thread.Sleep(Settings.Default.WaitTimeout);
+            var text = Clipboard.GetText();
+            textBox1.AppendText(Environment.NewLine + text);
+            
+            Uri uri;
+            if (!Uri.TryCreate(text, UriKind.Absolute, out uri))
+            {
+                text = "https://www.google.ru/search?q="+ text.Replace(" ", "+");
+            }
+            
+            Process.Start(text);
+        }
+
+        private void Form_Deactivate(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void notifyIcon_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+            }
+        }
+    }
+}
